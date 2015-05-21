8e611a1d907b421879305db11060749753d8b11f
Gustavo Narea <gustavonarea@2degreesnetwork.com>
2015-05-21 16:04:57 +0100

    Implemented project skeleton

---
+++ setup.py
@@ -0,0 +1,50 @@
+##############################################################################
+#
+# Copyright (c) 2015, 2degrees Limited.
+# All Rights Reserved.
+#
+# This file is part of drf-nested-resources
+# <https://github.com/2degrees/drf-nested-resources>, which is subject to the
+# provisions of the BSD at
+# <http://dev.2degreesnetwork.com/p/2degrees-license.html>. A copy of the
+# license should accompany this distribution. THIS SOFTWARE IS PROVIDED "AS IS"
+# AND ANY AND ALL EXPRESS OR IMPLIED WARRANTIES ARE DISCLAIMED, INCLUDING, BUT
+# NOT LIMITED TO, THE IMPLIED WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST
+# INFRINGEMENT, AND FITNESS FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+from os import path
+
+from setuptools import setup
+
+_HERE = path.abspath(path.dirname(__file__))
+_VERSION = open(path.join(_HERE, 'VERSION.txt')).readline().rstrip()
+_README = open(path.join(_HERE, 'README.rst')).read().strip()
+_CHANGELOG = open(path.join(_HERE, 'CHANGELOG.txt')).read().strip()
+_LONG_DESCRIPTION = '\n\n'.join((_README, _CHANGELOG))
+
+
+setup(
+    name='drf-nested-resources',
+    version=_VERSION,
+    description='Support for nested routes in the Django REST Framework',
+    long_description=_LONG_DESCRIPTION,
+    url='https://pypi.python.org/pypi/drf-nested-resources',
+    author='2degrees Limited',
+    author_email='2degrees-floss@googlegroups.com',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries',
+        ],
+    keywords='',
+    license='BSD (http://dev.2degreesnetwork.com/p/2degrees-license.html)',
+    include_package_data=True,
+    exclude_package_data={'': ['README.rst', 'CHANGELOG.txt']},
+    install_requires=['djangorestframework == 3.1.2'],
+    )
